ec4a465aca75a21ba7d1bb41d8048d6cdb4e06cb
daiyy <dyiyang@amazon.com>
2024-01-06 01:27:37 +0000

    add ks-tool code base

---
+++ ks-tool/src/main.rs
@@ -0,0 +1,96 @@
+mod utils;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use clap::{Parser, Subcommand};
+use clap::Args;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Debug)]
+#[derive(Args)]
+#[group(required = true, multiple = false)]
+struct Input {
+    /// ks input file
+    #[arg(short, long)]
+    ks: Option<String>,
+
+    /// merged CSV file of S3 inventory
+    #[arg(short, long)]
+    inventory: Option<String>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+
+    /// Read from local ks file or inventory CSV file, split to target number of prefix segments
+    Split {
+        #[command(flatten)]
+        input: Input,
+
+        /// target count of prefix segments to split
+        #[arg(short, long)]
+        count: usize,
+
+        /// output ks hints file
+        /// expected file name convention: {region}_{bucket}_ks_hints.input
+        #[arg(short, long, verbatim_doc_comment)]
+        output: String,
+    },
+
+    /// Download inventory from S3, merge all inventory files into a single local CSV file
+    Inventory {
+        /// region of S3 inventory
+        #[arg(short, long)]
+        region: String,
+
+        /// S3 URI of inventory manifest.json
+        /// format - S3://{bucket}/{dest_prefix}/{src_bucket}/{config_id}/{YYYY-MM-DDTHH-MMZ}/manifest.json
+        #[arg(short, long, verbatim_doc_comment)]
+        manifest: String,
+
+        /// local temp dir for downloaded S3 inventory files
+        #[arg(short, long)]
+        temp: String,
+
+        /// output merged CSV file
+        #[arg(short, long)]
+        output: String,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<(), tokio::io::Error> {
+
+    let quit = Arc::new(AtomicBool::new(false));
+    let q = quit.clone();
+    ctrlc::set_handler(move || {
+        q.store(true, Ordering::SeqCst);
+    }).expect("failed to setting ctrl-c signal handler");
+    let cli = Cli::parse();
+
+    match &cli.cmd {
+        Commands::Split { input, count, output } => {
+            if input.ks.is_some() {
+                let ks_file = input.ks.as_ref().unwrap();
+                utils::handle_ks_input(quit.clone(), ks_file, *count, &output).await?;
+            } else if input.inventory.is_some() {
+                let inventory_input = input.inventory.as_ref().unwrap();
+                utils::handle_inventory_input(quit.clone(), inventory_input, *count, &output).await?;
+            }
+        },
+        Commands::Inventory { region, manifest, temp, output } => {
+            let _ = region;
+            let _ = manifest;
+            let _ = temp;
+            let _ = output;
+            unimplemented!("SubCmd inventory");
+        },
+    }
+    Ok(())
+}
